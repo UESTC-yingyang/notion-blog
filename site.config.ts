275965e6693c6b0eb03ab9087bd101eee10b1bf0
HuangmingyuanUestc <49358630+HuangmingyuanUestc@users.noreply.github.com>
2022-07-06 17:51:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: 'https://avatars.githubusercontent.com/u/49358630?v=4',
-  defaultPageCover: null,
+  defaultPageCover: 'https://avatars.githubusercontent.com/u/49358630?v=4',
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
